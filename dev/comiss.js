bb9af7922b80f5a227d4e3a4f8f374d76846b6dd
Guilherme Ranoya <ranoya@gmail.com>
2024-07-10 20:52:06 -0300

    nova comissão

--- dev/comiss.js
+++ dev/comiss.js
@@ -1,47 +1,40 @@
 let montalista = function (arr, crit) {
-    
-    let code = `<div>`;
+  let code = `<div>`;
 
-    if (typeof arr["Líder " + crit] != "undefined" && arr["Líder " + crit] != "") {
-              code += `<b>${arr["Líder " + crit]}</b><br>`;
+  if (
+    typeof arr["Líder " + crit] != "undefined" &&
+    arr["Líder " + crit] != ""
+  ) {
+    code += `<b>${arr["Líder " + crit]}</b><br>`;
+  }
+
+  if (typeof arr[crit] != "undefined" && arr[crit] != "") {
+    let listas = arr[crit].split(",");
+    for (let i = 0; i < listas.length; i++) {
+      code += listas[i].trim() + "<br>";
     }
-
-    if (typeof arr[crit] != "undefined" && arr[crit] != "") {
-
-            let listas = arr[crit].split(",");
-            for (let i = 0; i < listas.length; i++) {
-                code += listas[i].trim() + "<br>";
-            }
-        
-    } else  {
-        code += "<br>";
-    }
-    code += `</div>`;
-    return code;
-    
-}
-
-
+  } else {
+    code += "<br>";
+  }
+  code += `</div>`;
+  return code;
+};
 
 let comiss = function (xpar) {
-
   let jsonfile = `https://docs.google.com/spreadsheets/d/1tjmcRwo4mLUYKrlitWA1c41T8iWebQcpBjEw77Y5LpU/edit#gid=665266084`;
 
-  getcsvdata(GoogleSheetCsvURL(jsonfile), function(dados) {
-          
-          let par = xpar;
-        
-          if (xpar.match(/atual/i)) {
-            
-              let anos = unique(dados, "ano");
-              par = Math.max(...anos).toString();
+  getcsvdata(GoogleSheetCsvURL(jsonfile), function (dados) {
+    let par = xpar;
 
-        }
-          
-        let newarr = select(dados, multipatterncheck_exclude, par);
-        let crit = "";
-        let listas = [];
-          let xpto = `
+    if (xpar.match(/atual/i)) {
+      let anos = unique(dados, "ano");
+      par = Math.max(...anos).toString();
+    }
+
+    let newarr = select(dados, multipatterncheck_exclude, par);
+    let crit = "";
+    let listas = [];
+    let xpto = `
 
         
       
@@ -49,7 +42,7 @@ let comiss = function (xpar) {
             .gradecomis {
                 display: grid;
                 gap: 16px 16px;
-                grid-template-columns: [init] 0.5fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr [fim];
+                grid-template-columns: [init] 0.5fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr [fim];
                 font-size: 14px;
                 min-width: 1400px;
                 overflow-x: scroll;
@@ -109,41 +102,39 @@ let comiss = function (xpar) {
         </style>
       
         <div class="comissmenu"><a class='linksmod'href='javascript:setinput("/comiss ")'>TODOS OS ANOS</a>`;
-        
-          let todosanos = unique(dados, "ano");
-          
-          for (let k = todosanos.length - 1; k >= 0; k--) {
-              xpto += `<a class='linksmod' href='javascript:setinput("/comiss ${todosanos[k]} ")'>${todosanos[k]}</a>`;
-          }
 
-          xpto += '</div>';
-          
-          
-        xpto += `
+    let todosanos = unique(dados, "ano");
+
+    for (let k = todosanos.length - 1; k >= 0; k--) {
+      xpto += `<a class='linksmod' href='javascript:setinput("/comiss ${todosanos[k]} ")'>${todosanos[k]}</a>`;
+    }
+
+    xpto += "</div>";
+
+    xpto += `
         <div class="outputgrid gradecomis">
         <div></div>
-        <div class='tablehead'>Coordenação</div><div class='tablehead'>Processo Seletivo</div><div class='tablehead'>Auto-Avaliação</div><div class='tablehead'>Credenciamento</div><div class='tablehead'>Editorial Fronteiras do Design</div><div class='tablehead'>Organização dos Seminários</div><div class='tablehead'>Coleta Capes</div><div class='tablehead'>Bolsas</div><div class='tablelinha'></div>`;
+        <div class='tablehead'>Coordenação</div><div class='tablehead'>Processo Seletivo</div><div class='tablehead'>Auto-Avaliação</div><div class='tablehead'>Gestão</div><div class='tablehead'>Credenciamento</div><div class='tablehead'>Editorial Fronteiras do Design</div><div class='tablehead'>Organização dos Seminários</div><div class='tablehead'>Coleta Capes</div><div class='tablehead'>Bolsas</div><div class='tablelinha'></div>`;
 
-      for (let i = 0; i < newarr.length; i++) {
-          
-          xpto += `<div class='tabelanos'>${newarr[i].ano}</div>`;
+    for (let i = 0; i < newarr.length; i++) {
+      xpto += `<div class='tabelanos'>${newarr[i].ano}</div>`;
 
-          // Coordenação
+      // Coordenação
 
-          xpto += montalista(newarr[i], "Coordenação");
-          xpto += montalista(newarr[i], "Seleção");
-          xpto += montalista(newarr[i], "Auto Avaliação");
-          xpto += montalista(newarr[i], "Credenciamento");
-          xpto += montalista(newarr[i], "Editorial Fronteiras");
-          xpto += montalista(newarr[i], "Oranização Seminários");
-          xpto += montalista(newarr[i], "Coleta Capes");
-          xpto += montalista(newarr[i], "Bolsas");
+      xpto += montalista(newarr[i], "Coordenação");
+      xpto += montalista(newarr[i], "Seleção");
+      xpto += montalista(newarr[i], "Auto Avaliação");
+      xpto += montalista(newarr[i], "Gestão");
+      xpto += montalista(newarr[i], "Credenciamento");
+      xpto += montalista(newarr[i], "Editorial Fronteiras");
+      xpto += montalista(newarr[i], "Oranização Seminários");
+      xpto += montalista(newarr[i], "Coleta Capes");
+      xpto += montalista(newarr[i], "Bolsas");
 
-          xpto += `<div class='tablelinha'></div>`;
-          
-      }
+      xpto += `<div class='tablelinha'></div>`;
+    }
 
-      xpto += `</div></div>`;
-      present(xpto);
-    });
+    xpto += `</div></div>`;
+    present(xpto);
+  });
 };
